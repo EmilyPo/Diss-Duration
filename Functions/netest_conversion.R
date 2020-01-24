acbe1059e121781aff4d00f8da5983d97718136b
EmilyPo <emilypo@uw.edu>
2020-01-24 11:19:03 -0800

    network troubleshooting rmd

--- Functions/netest_conversion.R
+++ Functions/netest_conversion.R
@@ -296,3 +296,202 @@ ee.netest <- function (x,y, set.formula.nw=TRUE, start.net=NULL) {
   class(out) <- "netest"
   return(out)
 }
+
+
+ee.netest.emily <- function (x,y, set.formula.nw=TRUE, start.net=NULL) {
+  
+  ## the netest object contains
+  ##[1] "fit"                "formation"          "target.stats"       "target.stats.names" "coef.form"          "coef.form.crude"    "coef.diss"
+  ##[8] "constraints"        "edapprox"
+  
+  
+  ##Initialize output list
+  out <- list()
+  
+  ## fit is an ergm object that contains.
+  ##[1] "coef"          "sample"        "iterations"    "MCMCtheta"     "loglikelihood" "gradient"      "hessian"       "covar"
+  ##[9] "failure"       "network"       "newnetworks"   "newnetwork"    "coef.init"     "est.cov"       "coef.hist"     "stats.hist"
+  ##[17] "steplen.hist"  "control"       "etamap"        "formula"       "target.stats"  "target.esteq"  "constraints"   "reference"
+  ##[25] "estimate"      "offset"        "drop"          "estimable"     "null.lik"
+  
+  ##Delete egodata
+  x$egodata<-NULL
+  
+  ##Assign the ergm.ego object to fit in netest
+  out$fit <- x
+  class(out$fit) <- "ergm"
+  
+  ##Adjust the ergm coeficient to get rid of the network size adjustment
+  out$fit$coef<-x$coef[2:length(x$coef)]
+  out$fit$coef[1]<-out$fit$coef[1] + x$coef[1]
+  
+  #Remove the netsize.adj colum from "sample"
+  ncol<-length(x$sample[1,])
+  out$fit$sample<-x$sample[,-1]
+  
+  #Iterations is copied directly from the ergm.ego fit to the netest fit object.
+  out$fit$iterations <- x$iterations
+  
+  ##Adjust the MCMCtheta coeficient to get rid of the network size adjustment
+  out$fit$MCMCtheta<-x$MCMCtheta[2:length(x$MCMCtheta)]
+  out$fit$MCMCtheta[1]<-out$fit$MCMCtheta[1] + x$MCMCtheta[1]
+  
+  
+  #loglikelihood is copied directly from the ergm.ego fit to the netest fit object.
+  out$fit$loglikelihood <- x$loglikelihood
+  
+  ##Remove the first element (NA) of gradient associated with network size adjust.
+  out$fit$gradient<-x$gradient[-1]
+  
+  ##Remove row 1 and col 1 for network size adjust from hessian matrix.
+  ncol <- length(x$hessian[1,])
+  out$fit$hessian <- x$hessian[2:ncol,2:ncol]
+  
+  ##Remove row 1 and col 1 for network size adjust from covar matrix.
+  ncol <- length(x$covar[1,])
+  out$fit$covar <- x$covar[2:ncol,2:ncol]
+  
+  #failure is copied directly from the ergm.ego fit to the netest fit object.
+  out$fit$failure <- x$failure
+  
+  #network, newnetworks and newnetwork are copied directly from the ergm.ego fit to the netest fit object.
+  out$fit$network <- x$network
+  out$fit$newnetworks <- x$newnetworks
+  out$fit$newnetwork <- x$newnetwork
+  
+  #coef.init is adjusted to remove the netsize.adj and adjust the edges term
+  out$fit$coef.init<-x$coef.init[2:length(x$coef.init)]
+  out$fit$coef.init[1]<-out$fit$coef.init[1] + x$coef.init[1]
+  
+  #est.cov: Remove row 1 and col 1 for network size adjust from est.cov
+  ncol <- length(x$est.cov[1,])
+  out$fit$est.cov <- x$est.cov[2:ncol,2:ncol]
+  
+  #coef.hist: combine edges and nesize adjustment, drop netsize adjustment. 
+  # THIS CHANGES THE COEF FOR THE THIRD ITEM, IT DOESN'T CHANGE THE EDGES. CHANGING to edit the edges term. 
+  ncol <- length(x$coef.hist[1,])
+  #x$coef.hist[,3] <- x$coef.hist[,2] + x$coef.hist[,3]
+  x$coef.hist[,2] <- x$coef.hist[,1] + x$coef.hist[,2]
+  out$fit$coef.hist <- x$coef.hist[,-1]
+  
+  #stats.hist: drop netsize adjustment.
+  out$fit$stats.hist <- x$stats.hist[,-1]
+  
+  #steplen.hist: Direct carryover
+  out$fit$steplen.hist <- x$steplen.hist
+  
+  #control:  Merge edges and netsize.adj in $init
+  out$fit$control <- x$control
+  out$fit$control$init <- x$control$init[-1]
+  out$fit$control$init[1] <- x$control$init[1] + x$control$init[2]
+  
+  #etamap: drop first element of each attribute
+  out$fit$etamap <- x$etamap
+  out$fit$etamap$cononical <- x$etamap$cononical[-(length(x$etamap$cononical))]
+  out$fit$etamap$offsetmap <- x$etamap$offsetmap[-1]
+  out$fit$etamap$offset <- x$etamap$offset[-1]
+  out$fit$etamap$offsettheta <- x$etamap$offsettheta-1
+  out$fit$etamap$curved <- x$etamap$curved
+  out$fit$etamap$etalength <- x$etamap$etalength - 1
+  
+  #formula
+  out$fit$formula <- x$formula
+  z<-(as.formula("nw ~ o"))
+  out$fit$formula[2] <- z[2]
+  
+  #JKB 4/24/18 - nw object needs to be added to the environment of the formation formula
+  #patterned after netest() lines 182-183
+  #DTH 4/46/2018 - testing cause of Males-Male ties
+  # deleting all ties to creat an empty network
+  
+  #JKB 6/5/18: allow for an alternate starting network
+  if (is.null(start.net)) {
+    nw <- x$network
+  } else nw <- start.net
+  count <- network.edgecount(nw)
+  delete.edges(nw,1:count)
+  if (set.formula.nw) environment(out$fit$formula) <- environment()
+  
+  
+  #target stats: drop the first element for netsizw.adj
+  out$fit$target.stats <- x$target.stats[-1]
+  
+  #target.esteq: Direct copy
+  out$fit$target.esteq <- x$target.esteq
+  
+  #constraints: Direct copy
+  out$fit$constraints <- x$constraints
+  
+  #reference: Direct copy
+  out$fit$reference <- x$reference
+  
+  #estimate: Direct copy
+  out$fit$estimate <- x$estimate
+  
+  #offset: Drop the first element
+  out$fit$offset <- x$offset[-1]
+  
+  #drop: Drop the first element
+  out$fit$drop <- x$drop[-1]
+  
+  #estimable: Drop the first element
+  out$fit$estimable <- x$estimable[-1]
+  
+  #null.lik: Direct copy
+  # out$fit$null.lik <- x$null.lik[-1]
+  # the above doesn't copy the null.lik - do we want this to be `numeric(0)`?
+  out$fit$null.lik <- x$null.lik[1]
+  
+  ## the netest object contains
+  ##[1] "fit"                "formation"          "target.stats"       "target.stats.names" "coef.form"          "coef.form.crude"    "coef.diss"
+  ##[8] "constraints"        "edapprox"
+  
+  #The objects other than fit.
+  
+  formation <- x$formula[-2]
+  target.stats <- as.numeric(x$target.stats[2:length(x$target.stats)])
+  # JKB 4/25/18 - changed next 2 lines, which turned target.stats into integer(0)
+  # I'm presuming the goal was to remove the offset term(s)
+  # DTH This works for me
+  
+  target.stats <- target.stats[!out$fit$offset]
+  target.stats.names <- names(out$fit$target.stats)[!out$fit$offset]
+  
+  coef.form.crude <- x$coef[2:length(x$coef)]
+  coef.form <- coef.form.crude
+  coef.form[1]<-coef.form[1]+x$coef[1]
+  coef.form.crude <- coef.form
+  
+  if (y$coef.adj[1] != -Inf){coef.form[1]<- coef.form[1] - y$coef.adj[1]}
+  
+  
+  constraints <- x$constraints
+  edapprox <- TRUE
+  
+  
+  ##assighn the elements of the netest object
+  out$formation <- formation
+  out$target.stats <- target.stats
+  
+  ##This is redundant with the above section on target stats.
+  
+  #if (length(names(out$fit$coef)) == length(target.stats)) {
+  #    out$target.stats.names <- names(out$fit$coef)
+  #}
+  #else {
+  #    out$target.stats.names <- names(out$fit$coef)[!out$fit$offset]}
+  
+  out$target.stats.names <- target.stats.names
+  
+  
+  out$coef.form <- coef.form
+  out$dissolution <- y$dissolution
+  out$coef.diss <- y
+  out$constraints <- constraints
+  out$edapprox <- edapprox
+  
+  
+  
+  class(out) <- "netest"
+  return(out)
+}
